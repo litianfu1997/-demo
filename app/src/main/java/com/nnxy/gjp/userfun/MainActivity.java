cc3a6f8a235e41fc32d3bd26d61432a9fb5b8e83
litianfu <1035869369@qq.com>
2019-04-26 14:56:12 +0800

    "完成登录界面的功能"

--- app/src/main/java/com/nnxy/gjp/userfun/MainActivity.java
+++ app/src/main/java/com/nnxy/gjp/userfun/MainActivity.java
@@ -1,7 +1,9 @@
-package com.nnxy.gjp;
+package com.nnxy.gjp.userfun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.nnxy.gjp.R;
 
 public class MainActivity extends AppCompatActivity {
 
