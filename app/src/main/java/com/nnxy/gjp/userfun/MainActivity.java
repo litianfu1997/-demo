97d70e2bba3a6191eb59907135be5f85cbb09e3d
litianfu <1035869369@qq.com>
2019-04-26 15:05:20 +0800

    完成主页面的注册按钮，点击跳转到注册页面的功能

--- app/src/main/java/com/nnxy/gjp/userfun/MainActivity.java
+++ app/src/main/java/com/nnxy/gjp/userfun/MainActivity.java
@@ -1,7 +1,9 @@
 package com.nnxy.gjp.userfun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.nnxy.gjp.R;
 
@@ -11,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+
+    /**
+     * 该按钮实现的功能是跳转到注册页面
+     * @param view
+     */
+    public void Intent_reg(View view){
+        startActivity(new Intent(MainActivity.this,RegisterActivity.class));
     }
 }
